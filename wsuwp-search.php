f2bfba04572dd448c659488f657ad7fbd0f2fe86
Jeremy Felt <jeremy.felt@gmail.com>
2016-11-03 11:07:52 -0700

    Add an initial WP-CLI command for ES index management
    `wp wsues reindex all` loops through all posts and pages and
    reindexes the content.
    
    `wp wsues reindex #` reindexes the content of one post or page.

--- wsuwp-search.php
+++ wsuwp-search.php
@@ -16,6 +16,10 @@ if ( ! defined( 'WPINC' ) ) {
 // The core plugin class.
 require dirname( __FILE__ ) . '/includes/class-wsu-search.php';
 
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	require_once( dirname( __FILE__ ) . '/includes/class-wp-cli-wsu-search-command.php' );
+}
+
 add_action( 'after_setup_theme', 'WSUWP_Search' );
 /**
  * Start things up.
